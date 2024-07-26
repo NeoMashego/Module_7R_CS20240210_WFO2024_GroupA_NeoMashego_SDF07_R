88324aba94d5cf62c01f2864e9aee218386a18a7
NeoMashego <mashegonoj@gmail.com>
2024-07-26 15:40:22 +0200

    Added js to bring user to different links on page

--- style.js
+++ style.js
@@ -1,5 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav_link');    //nav will look for respective tags onn page to take the user to
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');     //Open nav everytime I click
 }); 
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
